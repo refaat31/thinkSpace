a34ea2552c7ec74267f92770628b6f9b9c39a188
refaat31 <refaatalamgi@iut-dhaka.edu>
2020-08-02 12:48:39 +0600

    Validation for resgistration added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/users
-// @desc    test route
+// @route   POST api/users
+// @desc    register users
 // @access  public
-router.get("/", (req, res) => res.send("user route"));
+router.post(
+  "/",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "Email is invalid").isEmail(),
+    check(
+      "password",
+      "please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    console.log(req.body); //for this to work, u have to initialize the middleware for body parser
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors:errors.array()});
+    }
+    res.send("user route");
+  }
+);
 module.exports = router;
